f27c65bbe332b3c16c9f7aae08165ce4bdb34b7f
ArtGrohe <artgrohe@googlemail.com>
2018-07-25 19:46:08 +0200

    reactive forms

--- src/app/reactive/reactive.component.ts
+++ src/app/reactive/reactive.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class ReactiveComponent implements OnInit {
+  myForm: FormGroup
 
   constructor() { }
 
+  onSubmit(){
+    console.log(this.myForm);
+  }
+
   ngOnInit() {
+    this.myForm = new FormGroup({
+      'username': new FormControl(null, Validators.required),
+      'email': new FormControl(null, [
+        Validators.required,
+        Validators.pattern("^((([!#$%&'*+\-/=?^_`{|}~\w])|([!#$%&'*+\-/=?^_`{|}~\w][!#$%&'*+\-/=?^_`{|}~\.\w]{0,}[!#$%&'*+\-/=?^_`{|}~\w]))[@]\w+([-.]\w+)*\.\w+([-.]\w+)*)$")
+      ]),
+      'password': new FormControl(null, Validators.required)
+    });
   }
 
 }
